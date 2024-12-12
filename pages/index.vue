af6bccb1b45e379f175af465316a85aebc15a8c4
Nir Galon <nir@galons.io>
2024-12-12 10:51:28 +0200

    fix: more

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,42 @@
 <script setup lang="ts">
+import PDLJS from "peopledatalabs";
 const { $thewebco } = useNuxtApp();
 
 const email = ref();
+const loading = ref(false);
 const canEnter = ref(false);
 const visitorId = ref("");
+const visitorData = ref([] as Array<Record<string, any>>);
 let thewebcoClient: any = null;
+const PDLClient = new PDLJS({ apiKey: "1d97fc9523f88349ba530383c187c5066bfd8e3787fde9c3a600497bcd0e5a81" });
 
 onMounted(() => (thewebcoClient = $thewebco()));
 
 const emails = ["shay@ynet.co.il", "gil-b@ynet.co.il", "dror-c@ynet.co.il", "asaf@ynet.co.il", "avi@yit.co.il", "iris-k@ynet.co.il", "sivan-f@ynet.co.il"];
-const check = () => {
+const check = async () => {
+  loading.value = true;
   if (emails.includes(email.value)) {
-    canEnter.value = true;
     if (thewebcoClient) {
       visitorId.value = thewebcoClient.visitorId;
+
+      const response = await PDLClient.person.search.sql({
+        dataset: "email",
+        searchQuery: `SELECT * FROM person WHERE (emails.address = '${email.value}')`,
+        size: 1,
+        pretty: true,
+      });
+      console.log("response", response);
+      if (response && response.data.length && response.data[0].profiles?.length) {
+        visitorData.value = response.data[0].profiles;
+      }
+
+      canEnter.value = true;
     }
   } else {
     canEnter.value = false;
   }
+
+  loading.value = false;
 };
 </script>
 
@@ -28,13 +47,18 @@ const check = () => {
 
       <form class="flex gap-4 justify-center">
         <UInput id="email" type="email" label="email" placeholder="email" v-model="email" class="w-1/2" />
-        <UButton label="Check" type="submit" @click="check" />
+        <UButton label="Check" type="submit" @click="check" :loading="loading" />
       </form>
     </UCard>
 
     <UCard v-else>
       <p>Congratulations! You're worthy ;)</p>
-      <p>Your visitor id is: {{ visitorId }}</p>
+      <p class="mt-2">Your visitor id is: {{ visitorId }}</p>
+      <div v-if="Object.keys(visitorData).length" class="flex flex-col space-y-2 mt-2">
+        <div v-for="(item, index) of visitorData" :key="index">
+          <a v-if="item.url" :href="`https://${item.url}`" target="_blank">profile: https://{{ item.url }}</a>
+        </div>
+      </div>
     </UCard>
   </div>
 </template>
