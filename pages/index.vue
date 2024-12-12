4f0855583700dffba97253ee1f43ec9b6740b138
Nir Galon <nir@galons.io>
2024-12-12 09:25:49 +0200

    fix: foo

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+const { $thewebco } = useNuxtApp();
+
 const email = ref();
 const canEnter = ref(false);
+const visitorId = ref("");
+let thewebcoClient: any = null;
+
+onMounted(() => (thewebcoClient = $thewebco()));
 
 const emails = ["shay@ynet.co.il", "gil-b@ynet.co.il", "dror-c@ynet.co.il"];
 const check = () => {
   if (emails.includes(email.value)) {
     canEnter.value = true;
+    if (thewebcoClient) {
+      visitorId.value = thewebcoClient.visitorId;
+    }
   } else {
     canEnter.value = false;
   }
@@ -17,14 +26,15 @@ const check = () => {
     <UCard v-if="!canEnter">
       <h2 class="text-xl mb-4">This is a test environment for some people, put in your email to check if your're worthy</h2>
 
-      <div class="flex gap-4 justify-center">
+      <form class="flex gap-4 justify-center">
         <UInput label="email" v-model="email" class="w-1/2" />
         <UButton label="Check" type="submit" @click="check" />
-      </div>
+      </form>
     </UCard>
 
     <UCard v-else>
       <p>Congratulations! You're worthy ;)</p>
+      <p>Your visitor id is: {{ visitorId }}</p>
     </UCard>
   </div>
 </template>
