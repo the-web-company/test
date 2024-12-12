0ddbe463e121c1bef71817dfb17b96772209409b
Nir Galon <nir@galons.io>
2024-12-12 13:49:13 +0200

    fix: more

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@ const { $thewebco } = useNuxtApp();
 
 const email = ref();
 const loading = ref(false);
-const canEnter = ref(false);
+const canEnter = ref(null as null | Boolean);
 const visitorId = ref("");
 const visitorData = ref([] as Array<Record<string, any>>);
 let thewebcoClient: any = null;
@@ -15,13 +15,13 @@ onMounted(() => (thewebcoClient = $thewebco()));
 const emails = ["shay@ynet.co.il", "gil-b@ynet.co.il", "dror-c@ynet.co.il", "asaf@ynet.co.il", "avi@yit.co.il", "iris-k@ynet.co.il", "sivan-f@ynet.co.il"];
 const check = async () => {
   loading.value = true;
-  if (emails.includes(email.value)) {
+  if (emails.includes(email.value.toLowerCase())) {
     if (thewebcoClient) {
       visitorId.value = thewebcoClient.visitorId;
 
       const response = await PDLClient.person.search.sql({
         dataset: "email",
-        searchQuery: `SELECT * FROM person WHERE (emails.address = '${email.value}')`,
+        searchQuery: `SELECT * FROM person WHERE (emails.address = '${email.value.toLowerCase()}')`,
         size: 1,
         pretty: true,
       });
@@ -42,16 +42,18 @@ const check = async () => {
 
 <template>
   <div class="h-full flex justify-center items-center">
-    <UCard v-if="!canEnter">
+    <UCard v-if="canEnter == null || canEnter == false">
       <h2 class="text-xl mb-4">This is a test environment for some people, put in your email to check if your're worthy</h2>
 
       <form class="flex gap-4 justify-center" @submit.prevent="check">
         <UInput id="email" type="email" label="email" placeholder="email" v-model="email" class="w-1/2" />
         <UButton label="Check" type="submit" :loading="loading" />
       </form>
+
+      <p v-if="canEnter == false">You can't enter, sorry..</p>
     </UCard>
 
-    <UCard v-else>
+    <UCard v-else-if="canEnter == true">
       <p>Congratulations! You're worthy ;)</p>
       <p class="mt-2">Your visitor id is: {{ visitorId }}</p>
       <div v-if="Object.keys(visitorData).length" class="flex flex-col space-y-2 mt-2">
