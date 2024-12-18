7745b7ba3a4bcd31772c7f994e6d260800ebbbf6
Nir Galon <nir@galons.io>
2024-12-18 15:17:54 +0200

    fix: add await

--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,8 @@ const visitorData = ref([] as Array<Record<string, any>>);
 let thewebcoClient: any = null;
 const PDLClient = new PDLJS({ apiKey: "1d97fc9523f88349ba530383c187c5066bfd8e3787fde9c3a600497bcd0e5a81" });
 
-onMounted(() => {
-  thewebcoClient = $thewebco();
+onMounted(async () => {
+  thewebcoClient = await $thewebco();
   if (thewebcoClient) {
     visitorId.value = thewebcoClient.visitorId;
   }
@@ -45,25 +45,26 @@ const check = async () => {
 
 <template>
   <div class="h-full flex justify-center items-center">
-    <UCard v-if="canEnter == null || canEnter == false">
+    <UCard>
       <p class="my-2">Your visitor id is: {{ visitorId }}</p>
 
-      <h2 class="text-xl mb-4">This is a test environment for some people, put in your email to check if your're worthy</h2>
+      <div v-if="canEnter == null || canEnter == false">
+        <h2 class="text-xl mb-4">This is a test environment for some people, put in your email to check if your're worthy</h2>
 
-      <form class="flex gap-4 justify-center" @submit.prevent="check">
-        <UInput id="email" type="email" label="email" placeholder="email" v-model="email" class="w-1/2" />
-        <UButton label="Check" type="submit" :loading="loading" />
-      </form>
+        <form class="flex gap-4 justify-center" @submit.prevent="check">
+          <UInput id="email" type="email" label="email" placeholder="email" v-model="email" class="w-1/2" />
+          <UButton label="Check" type="submit" :loading="loading" />
+        </form>
 
-      <p v-if="canEnter == false">You can't enter, sorry..</p>
-    </UCard>
-
-    <UCard v-else-if="canEnter == true">
-      <p>Congratulations! You're worthy ;)</p>
-      <p class="mt-2">Your visitor id is: {{ visitorId }}</p>
-      <div v-if="Object.keys(visitorData).length" class="flex flex-col space-y-2 mt-2">
-        <div v-for="(item, index) of visitorData" :key="index">
-          <a v-if="item.url" :href="`https://${item.url}`" target="_blank">profile: https://{{ item.url }}</a>
+        <p v-if="canEnter == false">You can't enter, sorry..</p>
+      </div>
+      <div v-else-if="canEnter == true">
+        <p>Congratulations! You're worthy ;)</p>
+        <p class="mt-2">Your visitor id is: {{ visitorId }}</p>
+        <div v-if="Object.keys(visitorData).length" class="flex flex-col space-y-2 mt-2">
+          <div v-for="(item, index) of visitorData" :key="index">
+            <a v-if="item.url" :href="`https://${item.url}`" target="_blank">profile: https://{{ item.url }}</a>
+          </div>
         </div>
       </div>
     </UCard>
--- plugins/thewebco.client.js
+++ plugins/thewebco.client.js
@@ -1,8 +1,8 @@
 import { defineNuxtPlugin } from "#app";
 import { TheWebCo } from "https://storage.googleapis.com/thewebco.cloud/TheWebCo.js";
 
-export default defineNuxtPlugin(() => {
-  const thewebcoClient = new TheWebCo("268c452f-b153-4a3c-bb9c-77fa71283d51");
+export default defineNuxtPlugin(async () => {
+  const thewebcoClient = await new TheWebCo("268c452f-b153-4a3c-bb9c-77fa71283d51");
 
   // Make sure that pageviews are captured with each route change
   const router = useRouter();
